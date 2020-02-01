927ed90bb4381cd0bf50daf03b59c06599a2b094
sarbot <mail@sarbot.de>
2020-02-01 01:45:28 +0100

    default avatar

---
+++ templates/telegrambot/__init__.py
@@ -0,0 +1,22 @@
+version = '0.0.1'
+author = 'Telebotter'
+
+# some configs to automaticly add bot to the website 
+# publicbot = True
+
+# set url if bot has a website
+# url = 'https://telebotter.sarbot.de/app_name'
+
+# set bot nick name (shown in contacts)
+# nickname = 'App Name Bot'
+
+# set bot user name (the one used for @mentions)
+# username = 'app_name_bot'
+
+# description of the bot
+# description = "Kurzbeschreibung des Bots. ~1 (Ab)Satz."
+
+# help text, shown on the help page above the list of commands
+# helptext = "Fuege den Bot einer Gruppe hinzu. Er muss Admin sein."
+
+
