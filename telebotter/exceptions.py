947401a0f1989b4b654e665f1218e7fe4eb4da7f
Lukas <mail@sarbot.de>
2020-04-19 13:44:15 +0200

    common exceptions

---
+++ telebotter/exceptions.py
@@ -0,0 +1,7 @@
+"""Collection of errors that can be usefull in any cases.
+Bots can also have there custom Errors, but if one of these fit, you should
+use the error from this module.
+"""
+
+class ArgumentError(Exception):
+    pass
